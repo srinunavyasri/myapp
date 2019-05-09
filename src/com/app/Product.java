ea896606c7bdec2fed565bc64ce2efb603a962c6
SRINU <SRINU@DESKTOP-CP0169F>
2019-05-09 19:05:22 +0530

    1st file change

---
+++ src/com/app/Product.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Product {
+	private int prodCost;
+
+	public int getProdCost() {
+		return prodCost;
+	}
+
+	public void setProdCost(int prodCost) {
+		this.prodCost = prodCost;
+	}
+	
+
+}
